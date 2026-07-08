21f8a7119e09254f3add269f3cd62f2ee4a07dd8
jayce <contact@jaycebaxter.com>
2026-07-08 10:31:52 -0400

    Added project 1

---
+++ 1_HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+        
+    int a = 2;
+    int b = 14;
+
+    printf("Hello! My name is Jayce, I live in Ontario and I am %d years old.\nI have a cat named Lola and I think C is really cool!", a * b);
+}
